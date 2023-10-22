789a38b0194d747534ae68bbfb7edfb21d5a1094
Jonas Hinterdorfer <138358038+JonasHinterdorfer@users.noreply.github.com>
2023-10-22 18:34:13 +0200

    Delete Script/Server directory

--- Script/Server/Server/Data/GameData.cs
+++
@@ -1,9 +0,0 @@
-﻿using Server.Request;
-
-namespace Server.Data
-{
-    public class GameData
-    {
-        public static List<Game> Games = new();
-    }
-}
--- Script/Server/Server/Request/IRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Server.Request
-{
-    public interface IRequest
-    {
-        public bool Error { get; set; }
-        public string ErrorMessage { get; set; }
-    }
-}
